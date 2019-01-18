0c07d95743ae9cbc8d2b93c302460841acc22b87
Oliver <oliver.grant@gmail.com>
2019-01-18 23:05:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,4 +10,22 @@ def using_pop (array)
   array.pop
 end
 
-def pop_with_args()
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def usimg_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
+
+def using_insert(array,element)
+  array.insert(element)
+end
