5323edea1249ac2cfffdc05c2eb15b5857e45d91
Oliver <oliver.grant@gmail.com>
2019-01-18 23:00:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,12 @@ def using_push(array,string)
   array.push(string) #add to the end
 end
 
-def
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop (array)
+  array.pop
+end
+
+def pop_with_args()
